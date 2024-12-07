a543c2c461f745c7d11c9c8f8e385d9b5be84c0d
sshnote <springssh0504@naver.com>
2024-12-07 21:22:46 +0900

    first commit

---
+++ src/main/java/ssh/githubactionsbasic/GithubActionsBasicApplication.java
@@ -0,0 +1,13 @@
+package ssh.githubactionsbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ssh/githubactionsbasic/GithubActionsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package ssh.githubactionsbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
